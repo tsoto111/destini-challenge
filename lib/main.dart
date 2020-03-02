038e97bfa3f37ff15bcd210081d7cae77a4b2600
Tavo <tsoto111@gmail.com>
2020-03-01 23:08:40 -0600

    Hooking up new functions to the buttons for choice one and two

--- lib/main.dart
+++ lib/main.dart
@@ -58,8 +58,7 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.red,
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                    storyBrain.getChoiceOne(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
@@ -80,8 +79,7 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.blue,
                   child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 1.
-                    'Choice 2',
+                    storyBrain.getChoiceTwo(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
